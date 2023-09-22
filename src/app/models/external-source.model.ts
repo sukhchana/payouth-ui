2dcf83f0bc2175e47d88a1e889a3f5962168ec52
jakubborawski <jakub.borawski.praca@gmail.com>
2023-09-22 12:11:58 +0200

    Added page for external links

---
+++ src/app/models/external-source.model.ts
@@ -0,0 +1,6 @@
+export interface externalSourceModel {
+    title: string,
+    link: string,
+    description: string,
+    icon: string
+}
--- src/app/pages/how-to-vote-page/how-to-vote-page.component.ts
+++ src/app/pages/how-to-vote-page/how-to-vote-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ExternalSourcesService } from 'src/app/services/external-sources.service';
+import { externalSourceModel } from '../../models/external-source.model';
 
 @Component({
   selector: 'app-how-to-vote-page',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class HowToVotePageComponent {
 
+  mapEndpoint: string = "";
+  externalSources: externalSourceModel[] = [];
+
+  constructor (private externalSourcesService: ExternalSourcesService){
+    this.getExternalSources();
+    console.log(this.externalSources);
+  }
+  
+  getExternalSources(){
+    this.externalSources = this.externalSourcesService.getExternalSources();
+  }
+
+  navigateToExternalLink(link: string){
+    window.location.href = link;
+  }
+  /*
+  getMapServiceEndpoint(){
+    return this.appConfigService.getMapServiceEndpoint();
+  }*/
 }
