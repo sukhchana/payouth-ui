23d9555a26058d0dc56c744d9daeb63f4f7a4ef6
jakubborawski <jakub.borawski.praca@gmail.com>
2023-09-22 21:19:42 +0200

    Added registering user using API

---
+++ src/app/models/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+    firstName: string;
+    lastName: string;
+    email: string;
+    gender: string;
+    race: string;
+    county: string;
+    dateOfBirth: string;
+}
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,16 +1,36 @@
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { EmailCookieName } from '../models/cookie-names.model';
+import { User } from '../models/user';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  targetApiUrl = "https://igwx8jmmyz.us-east-1.awsapprunner.com";
 
-  constructor(private cookieService: CookieService) { }
+  constructor(private cookieService: CookieService, private http: HttpClient) { }
+
+  registerUser(user: User){
+    let body = JSON.stringify(user);
+    console.log("body", body);
+    this.executeJsonPost("/users", body).subscribe(res => {
+      this.cookieService.set(EmailCookieName, user.email);
+    });
+  }
 
   setUserEmail(email: string) {
     this.cookieService.set(EmailCookieName, email);
+  }
+
+  executeJsonPost(action: string, body: any): Observable<any> {
+    let url = this.targetApiUrl + action;
+    let headers = new HttpHeaders({
+        'Content-Type': 'application/json'
+    });
+    return this.http.post(url, body, { headers: headers });
   }
 
   getUserEmail() {
@@ -23,5 +43,5 @@ export class UsersService {
 
   logout(){
     this.cookieService.delete(EmailCookieName);
-  }
+  }  
 }
