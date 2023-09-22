3b2791fca2663555cf6462b109e142ca5bf0fe91
sukhchana <40441463+sukhchana@users.noreply.github.com>
2023-09-22 19:05:23 +0100

    Voter Registrations

---
+++ src/app/pages/voter-registrations/voter-registrations.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-voter-registrations',
+  templateUrl: './voter-registrations.component.html',
+  styleUrls: ['./voter-registrations.component.scss']
+})
+export class VoterRegistrationsComponent {
+
+}
